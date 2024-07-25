c1265617be03ef9974740e7736f8d1d258938190
Kedar Dabhadkar <kedar.dabhadkar@gmail.com>
2024-07-25 01:32:43 -0400

    Replace gpt-4 with gpt-4-turbo

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ import time
 
 from utils import derive_streaming_dash_app, add_streaming
 
+# Define a prompt globally
+prompt = ChatPromptTemplate.from_template("You are a helpful and kind AI assistant. Respond in less than 100 words. {user_prompt}")
+
 
 # Define a dummy Fast Dash app. We only need the automated layout
 # This app currenrly has a single string input and a single string output, but can be easily modified
@@ -22,48 +25,45 @@ def streaming_responses_with_fast_dash(prompt: str) -> (response_component,
                                                         response_component,
                                                         response_component):
     "Template for streaming responses with Fast Dash and LangChain"
-    gpt_3_5_turbo = gpt_4_turbo = llama_3_1 = "None"
-    return gpt_3_5_turbo, gpt_4_turbo, llama_3_1
+    gpt_4_turbo = gpt_4o = llama_3_1_7B = "None"
+    return gpt_4_turbo, gpt_4o, llama_3_1_7B
 
-app = FastDash(streaming_responses_with_fast_dash)
+app = FastDash(streaming_responses_with_fast_dash, theme="sandstone")
 app = derive_streaming_dash_app(app)
 
 
 # Streaming function 1
-def prompt_gpt_3_5_turbo(topic):
+def prompt_gpt_4_turbo(user_prompt):
 
-    prompt = ChatPromptTemplate.from_template("Write 100 words on {topic}")
-    model = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
-    parser = StrOutputParser()
-
-    chain = prompt | model | parser
-
-    return chain.stream({"topic": topic})
-
-def prompt_gpt_4_turbo(topic):
-
-    prompt = ChatPromptTemplate.from_template("Write 100 words on {topic}")
     model = ChatOpenAI(model="gpt-4-turbo", temperature=0)
     parser = StrOutputParser()
 
     chain = prompt | model | parser
 
-    return chain.stream({"topic": topic})
+    return chain.stream({"user_prompt": user_prompt})
+
+def prompt_gpt_4_turbo(user_prompt):
+
+    model = ChatOpenAI(model="gpt-4o", temperature=0)
+    parser = StrOutputParser()
+
+    chain = prompt | model | parser
+
+    return chain.stream({"user_prompt": user_prompt})
 
 
-def prompt_llama3_1(topic):
+def prompt_llama3_1(user_prompt):
 
-    prompt = ChatPromptTemplate.from_template("Write 100 words on {topic}")
     model = ChatTogether(model="meta-llama/Llama-3-70b-chat-hf")
     parser = StrOutputParser()
 
     chain = prompt | model | parser
     
-    return chain.stream({"topic": topic})
+    return chain.stream({"user_prompt": user_prompt})
 
 
 # Enable streaming in the app
-add_streaming(app, prompt_gpt_3_5_turbo, 1)
+add_streaming(app, prompt_gpt_4_turbo, 1)
 add_streaming(app, prompt_gpt_4_turbo, 2)
 add_streaming(app, prompt_llama3_1, 3)
 
